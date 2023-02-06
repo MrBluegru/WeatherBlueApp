383ecea775e7125bd6e8f98c6371f29e7817b951
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-02-06 13:44:19 -0300

    Add redux

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
+import { Provider } from "react-redux";
+import { store } from "./src/redux/store";
 import Navigation from "./Navigation";
 
 export default function App() {
-  return <Navigation />;
+  return (
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
+  );
 }
---
+++ src/redux/favoritesSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  favorites: [],
+};
+
+export const favoritesSlice = createSlice({
+  name: "favorites",
+  initialState,
+  reducers: {
+    setFavReducer: (state, action) => {
+      state.favorites = action.payload;
+    },
+    addFavReducer: (state, action) => {
+      state.favorites.push(action.payload);
+    },
+    deleteFavReducer: (state, action) => {
+      const id = action.payload;
+      state.favorites = state.favorites.filter((favC) => favorites.id !== id);
+    },
+  },
+});
+
+export const { setFavReducer, addFavReducer, deleteFavReducer } =
+  favoritesSlice.actions;
+
+export default favoritesSlice.reducer;
