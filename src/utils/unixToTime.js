506cc5756ed4f494541c23f4c0359555b1277a18
Mr. Blue <miguelrodriguezcastle@gmail.com>
2023-02-08 13:48:18 -0300

    Add string "Hs." at response

--- src/utils/unixToTime.js
+++ src/utils/unixToTime.js
@@ -1,7 +1,7 @@
 const unixToTime = (unix) => {
   const timeUnix = unix * 1000;
   const date = new Date(timeUnix);
-  return `${date.getHours()}:${date.getMinutes()}`;
+  return `${date.getHours()}:${date.getMinutes()} Hs.`;
 };
 
 export default unixToTime;
